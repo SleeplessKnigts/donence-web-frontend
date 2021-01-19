821c9f0c068b895e3c5c6ee71625987e76789d0d
Kerem <krmtrk20@gmail.com>
2021-01-19 16:32:26 +0300

    build project with create-react-app

---
+++ src/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { colors } from "./constants/theme";
+import { BrowserRouter } from "react-router-dom";
+
+const theme = extendTheme({ colors });
+
+ReactDOM.render(
+    <React.StrictMode>
+        <ChakraProvider theme={theme}>
+                <App />
+        </ChakraProvider>
+    </React.StrictMode>,
+    document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
