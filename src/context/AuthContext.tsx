8e3b5a7fe11fda22e564ab4069c674de2ff09441
Kerem <krmtrk20@gmail.com>
2021-01-21 20:15:44 +0300

    add logout functionality It just removes the token from local storage(nothing fancy)

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,34 +1,38 @@
 import React, { createContext, useEffect, useState } from "react";
 
 interface IAuth {
-    token: string,
-    isAuthenticated: boolean
-    authenticate: (token: string) => void
+    token: string;
+    isAuthenticated: boolean;
+    authenticate: (token: string) => void;
+    logout: () => void;
 }
 
 export const AuthContext = createContext<Partial<IAuth>>({});
 
-const AuthProvider: React.FC = ({children}) => {
+const AuthProvider: React.FC = ({ children }) => {
     const [token, setToken] = useState("");
     const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-    useEffect(()=> {
-        const token = localStorage.getItem("ACCESS_TOKEN")
-        if(token){
-            authenticate(token)
+    //Dependency list is empty because this useEffect should only work once(when the app mounts)
+    useEffect(() => {
+        const token = localStorage.getItem("ACCESS_TOKEN");
+        if (token) {
+            authenticate(token);
         }
-    })
+    }, []);
 
     const authenticate = (token: string) => {
         setToken(token);
         setIsAuthenticated(true);
-    }
+    };
 
-    return (
-        <AuthContext.Provider value={{token, isAuthenticated, authenticate}}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
+    const logout = () => {
+        setToken("");
+        setIsAuthenticated(false);
+        localStorage.removeItem("ACCESS_TOKEN");
+    };
+
+    return <AuthContext.Provider value={{ token, isAuthenticated, authenticate, logout }}>{children}</AuthContext.Provider>;
+};
 
 export default AuthProvider;
