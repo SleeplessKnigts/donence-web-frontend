84a1340b4b1b226b808bc67916df0182a5ee42a5
Kerem <krmtrk20@gmail.com>
2021-01-21 18:19:17 +0300

    add sample consumer for authentication context

--- src/components/molecules/Menu.tsx
+++ src/components/molecules/Menu.tsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useContext } from "react";
 import { Box, Button, Flex } from "@chakra-ui/react";
-import { Logo } from "../atoms/Logo"
+import { Logo } from "../atoms/Logo";
 import { MenuToggle, MenuToggleProps } from "../atoms/MenuToggle";
-import { MenuItem } from '../atoms/MenuItem';
+import { MenuItem } from "../atoms/MenuItem";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 
 export const Menu: React.FC<MenuToggleProps> = (props) => {
     const { isOpen, toggle } = props;
+    const { isAuthenticated } = useContext(AuthContext);
+
+    const authenticatedMenuElements = (
+        <>
+            <MenuItem to={"/very-authenticated-1"}>very-authenticated-1</MenuItem>
+            <MenuItem to={"/very-authenticated-1"}>very-authenticated-2</MenuItem>
+        </>
+    );
+
+
+
     return (
         <Flex as="nav" align="center" justify="space-between" wrap="wrap" padding="1rem" bg="brand.900" color="white">
             <Flex align="center" mr={5}>
@@ -21,6 +33,7 @@ export const Menu: React.FC<MenuToggleProps> = (props) => {
             >
                 <MenuItem to={"/haberler"}>Haberler</MenuItem>
                 <MenuItem to={"/loL"}>lol</MenuItem>
+                {isAuthenticated && authenticatedMenuElements}
             </Box>
 
             <Box display={{ sm: isOpen ? "block" : "none", md: "block" }} mt={{ base: 4, md: 0 }}>
