015f23ffa5961d2f6ee82b3e76aa1796bda8fa61
Arif <marfsahin@gmail.com>
2021-02-02 21:54:46 +0300

    remove unused imports
    -reformat with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import React, { useState } from "react";
-import { useContext } from "react-dom/node_modules/@types/react";
-import { useEffect } from "react-dom/node_modules/@types/react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { Menu } from "./components/molecules/Menu";
-import AuthProvider, { AuthContext } from "./context/AuthContext";
-import { Login } from "./pages/Login/Login";
-import { OAuth2RedirectHandler } from "./pages/Login/Oauth2RedirectHandler";
+import React, {useState} from "react";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {Menu} from "./components/molecules/Menu";
+import AuthProvider from "./context/AuthContext";
+import {Login} from "./pages/Login/Login";
+import {OAuth2RedirectHandler} from "./pages/Login/Oauth2RedirectHandler";
 import AddRecyclePoint from "./pages/AdminPanel/AddRecyclePoint";
 import RecyclePoint from "./pages/AdminPanel/RecyclePoint";
 import AdminPanel from "./pages/AdminPanel/AdminPanel";
@@ -20,13 +18,13 @@ function App() {
     return (
         <BrowserRouter>
             <AuthProvider>
-                <Menu isOpen={isOpen} toggle={handleToggle} />
+                <Menu isOpen={isOpen} toggle={handleToggle}/>
                 <Switch>
-                    <Route path="/oauth2/redirect" component={OAuth2RedirectHandler} />
-                    <Route exact path="/giris-yap" component={Login} />
-                    <Route exact path="/admin" component={AdminPanel} />
-                    <Route exact path="/admin/recycle-point" component={RecyclePoint} />
-                    <Route exact path="/admin/recycle-point/new" component={AddRecyclePoint} />
+                    <Route path="/oauth2/redirect" component={OAuth2RedirectHandler}/>
+                    <Route exact path="/giris-yap" component={Login}/>
+                    <Route exact path="/admin" component={AdminPanel}/>
+                    <Route exact path="/admin/recycle-point" component={RecyclePoint}/>
+                    <Route exact path="/admin/recycle-point/new" component={AddRecyclePoint}/>
                 </Switch>
             </AuthProvider>
         </BrowserRouter>
