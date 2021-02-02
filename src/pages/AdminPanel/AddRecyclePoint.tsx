c8661bae4ac97a2815b16452f5f2593f9d84acec
Arif <marfsahin@gmail.com>
2021-02-02 21:58:34 +0300

    add form into add-recycle-point component
    TODO implement api call

--- src/pages/AdminPanel/AddRecyclePoint.tsx
+++ src/pages/AdminPanel/AddRecyclePoint.tsx
@@ -1,15 +1,45 @@
+import {Button, Center, FormControl, Input} from "@chakra-ui/react";
+import React, {useState} from "react";
 import Map from "../../components/organisms/Map/Map";
-import {Input, Button, Center} from '@chakra-ui/react';
+import {useForm} from "react-hook-form";
 
 export const AddRecyclePoint = () => {
+    const [currentLoc, setCurrentLoc] = useState({});
+    const {handleSubmit, register, formState} = useForm();
+
+    function onSubmit(values: { name: string; location: {}; }) {
+        //TODO send query
+        values.location = currentLoc;
+        console.log(values);
+    }
+
     return (
-        <div>
-            <Map/>
-            <Input/>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <FormControl>
+                <Map setCurrentLoc={setCurrentLoc}/>
+            </FormControl>
+            <FormControl>
+                <Input
+                    m={2}
+                    name="name"
+                    placeholder="Description"
+                    focusBorderColor="lime"
+                    borderColor="green.700"
+                    isRequired={true}
+                    ref={register({required: true})}
+                />
+            </FormControl>
             <Center>
-                <Button colorScheme="teal">Add Point</Button>
+                <Button
+                    colorScheme="teal"
+                    m={2}
+                    isLoading={formState.isSubmitting}
+                    type="submit"
+                >
+                    Add Point
+                </Button>
             </Center>
-        </div>
+        </form>
     );
 }
 
