fa96c9cccbdd772d925467de31bcd0bbb06e4cb7
Kerem <krmtrk20@gmail.com>
2021-03-06 19:20:58 +0300

    add features component to contain feauture components

---
+++ src/containers/Features/Features.tsx
@@ -0,0 +1,19 @@
+import { Container, Flex, Heading, SimpleGrid } from "@chakra-ui/layout";
+import React from "react";
+import { ReactNode } from "react-dom/node_modules/@types/react";
+
+export interface FeaturesProps {
+    title: string;
+    children: ReactNode;
+}
+
+export const Features: React.FC<FeaturesProps> = ({ title, children }) => {
+    return (
+        <>
+            <Heading marginY="8" marginX="auto">{title}</Heading>
+            <SimpleGrid columns={{ base: 1, sm: 2, md: 4 }} spacing="8">
+                {children}
+            </SimpleGrid>
+        </>
+    );
+};
