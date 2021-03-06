a8c0b22e66c4fe103691637dcb97889ce4a9e6e8
Kerem <krmtrk20@gmail.com>
2021-03-06 19:21:12 +0300

    add home page

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,89 @@
+import { Button, ButtonGroup } from "@chakra-ui/button";
+import { Image } from "@chakra-ui/image";
+import { Box, Center, Container, Heading, Text } from "@chakra-ui/layout";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { FcAndroidOs } from "react-icons/fc";
+import { Link } from "react-router-dom";
+import LandingPageImage from "../../assets/ac29042cc76a0c2abb33ba12dc30f37f.png";
+import RecycleElements from "../../assets/Donence1.png";
+import Feature1 from "../../assets/features1.png";
+import Feature2 from "../../assets/features2.png";
+import Feature3 from "../../assets/features3.png";
+import Feature4 from "../../assets/features4.png";
+import { Feature } from "../../components/molecules/Feature";
+import { Features } from "../../containers/Features/Features";
+
+export const Home: React.FC = () => {
+    return (
+        <>
+            <Box position="relative" color="brand.800">
+                <Image
+                    src={LandingPageImage}
+                    alt="Landing Page Image"
+                    objectFit="cover"
+                    maxHeight="80vh"
+                    width="full"
+                    opacity="0.5"
+                />
+                <Box maxWidth="16rem" position="absolute" left="10%" top="8%">
+                    <Heading>Haydi sen de</Heading>
+                    <Text
+                        bgGradient="linear(to-r, #478ad2,#0f263d)"
+                        bgClip="text"
+                        fontSize="4xl"
+                        fontWeight="extrabold"
+                    >
+                        dönüştür.
+                    </Text>
+                </Box>
+            </Box>
+            <Container color="brand.800" maxWidth="container.lg">
+                <Heading marginY="8">Neden?</Heading>
+                <Feature
+                    title="Her gün daha da büyüyen dünyada atıkların yönetimi gayrı resmi ve dağıtık firmalar
+                            tarafından yönetilmektedir."
+                    description="Hal böyle olunca belediyeler ve daha büyük çaplı organizatörler geri dönüşümü efektif bir şekilde yönetmeyi başaramıyorlar. Dönence ise tek bir sistemden bütün verilere ve kaynaklara erişime izin vererek bu problemi çözmeyi amaçlıyor."
+                    imageSource={RecycleElements}
+                    direction="row"
+                />
+
+                <Features title="Çözümler">
+                    <Feature
+                        title="Haritalama"
+                        description="Kullanıcılar, bulundukları adres üzerinde farklı isteklerde bulanabilirler."
+                        imageSource={Feature1}
+                    />
+                    <Feature
+                        title="Farkındalık"
+                        description="Kullanıcılar, sağladıkları faydayı takip edebilirler."
+                        imageSource={Feature2}
+                    />
+                    <Feature
+                        title="Monitörleme"
+                        description="Belediyeler mahalle ölçeğinde geri dönüşüm verisi toplayabilirler."
+                        imageSource={Feature3}
+                    />
+                    <Feature
+                        title="Toplama"
+                        description="Belediyeler toplama noktaları oluşturup, kullanıcaların isteklerine cevap verebilirler."
+                        imageSource={Feature4}
+                    />
+                </Features>
+                <Center marginY="12">
+                    <ButtonGroup spacing="12" variant="outline" colorScheme="brand">
+                        <Button rightIcon={<FcAndroidOs />} borderRadius="full">
+                            İndirin
+                        </Button>
+                        <Link to="/giris-yap">
+                            <Button rightIcon={<FontAwesomeIcon icon={faUser} />} borderRadius="full">
+                                Üye olun
+                            </Button>
+                        </Link>
+                    </ButtonGroup>
+                </Center>
+            </Container>
+        </>
+    );
+};
