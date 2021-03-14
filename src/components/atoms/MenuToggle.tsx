5710c874cb1b569dfb08742b5bd666fa651fdfab
Kerem <krmtrk20@gmail.com>
2021-03-14 21:19:32 +0300

    add style to navbar toggle

--- src/components/atoms/MenuToggle.tsx
+++ src/components/atoms/MenuToggle.tsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import {Box} from '@chakra-ui/react'
+import React from "react";
+import { Box, IconButton } from "@chakra-ui/react";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export interface MenuToggleProps {
-    toggle: () => void,
-    isOpen: boolean,
+    toggle: () => void;
+    isOpen: boolean;
 }
 
 export const MenuToggle: React.FC<MenuToggleProps> = (props) => {
-    return(
-        <Box display={{base: "block", md: "none"}} onClick={props.toggle}>
-            {props.isOpen ? <p>Close</p> : <p>Open</p>} 
+    return (
+        <Box display={{ base: "block", md: "none" }} onClick={props.toggle}>
+            {props.isOpen ? (
+                <IconButton variant="outline" aria-label="Close" icon={<FontAwesomeIcon icon={faTimes} />} />
+            ) : (
+                <IconButton variant="outline" aria-label="Close" icon={<FontAwesomeIcon icon={faBars} />} />
+            )}
         </Box>
-    )
-}
+    );
+};
