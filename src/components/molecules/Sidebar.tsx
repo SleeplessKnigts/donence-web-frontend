874be47a51b85094d5c14a48cfe95ef3a9440913
Arif <marfsahin@gmail.com>
2021-03-17 15:37:57 +0300

    add sidebar molecule for admin panel

---
+++ src/components/molecules/Sidebar.tsx
@@ -0,0 +1,55 @@
+import { Box, Flex } from '@chakra-ui/layout';
+import {
+    Drawer,
+    DrawerBody,
+    DrawerCloseButton,
+    DrawerContent,
+    DrawerHeader,
+    DrawerOverlay,
+} from '@chakra-ui/modal';
+import { Logo } from '../atoms/Logo';
+import { SidebarContent } from '../atoms/SidebarContent';
+
+type SidebarProps = {
+    isOpen: boolean;
+    onClose: Function;
+    variant?: string;
+};
+
+export const Sidebar: React.FC<SidebarProps> = ({
+    isOpen,
+    onClose,
+    variant,
+}) => {
+    return variant === 'sidebar' ? (
+        <Box
+            position='fixed'
+            left={0}
+            p={5}
+            w='300px'
+            top={0}
+            h='100%'
+            bg='brand.900'
+            color='green.700'
+        >
+            <Flex align='center' mb={5}>
+                <Logo />
+            </Flex>
+            <SidebarContent />
+        </Box>
+    ) : (
+        <Drawer isOpen={isOpen} placement='left' onClose={() => onClose}>
+            <DrawerOverlay>
+                <DrawerContent>
+                    <DrawerCloseButton />
+                    <DrawerHeader>Chakra-UI</DrawerHeader>
+                    <DrawerBody>
+                        <SidebarContent />
+                    </DrawerBody>
+                </DrawerContent>
+            </DrawerOverlay>
+        </Drawer>
+    );
+};
+
+export default Sidebar;
