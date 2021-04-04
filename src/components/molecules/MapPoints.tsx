56812b869acc1027340f6d3d517da5090e2620c7
Kerem <krmtrk20@gmail.com>
2021-04-04 13:57:35 +0300

    add: recyclepointplacetype when admin adds a new recyclepoint

--- src/components/molecules/MapPoints.tsx
+++ src/components/molecules/MapPoints.tsx
@@ -1,6 +1,7 @@
 import { Box } from "@chakra-ui/layout";
 import { GoogleMap, InfoWindow, LoadScript, Marker } from "@react-google-maps/api";
 import React, { useState } from "react";
+import { TitleToColorMapping } from "../../constants/Mappings";
 import { RecyclePoint } from "../../shared/types";
 
 export interface MapPoinstProps {
@@ -19,10 +20,11 @@ export const MapPoints: React.FC<MapPoinstProps> = ({ points, center = { lat: 39
     return (
         <LoadScript googleMapsApiKey="AIzaSyAaMe1ol3asoFB2sHw0g1LlMq6CalKi9-Y">
             <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
-                {points?.map((el, index) => {
+                {points?.map((el: RecyclePoint, index) => {
                     return (
                         <Marker
                             key={index}
+                            icon={TitleToColorMapping[el.recyclyPointPlaceType]}
                             position={{ lat: el.lat, lng: el.lng }}
                             title={el.recyclePointDetail}
                             onClick={() => setPosition(el)}
--- src/pages/AdminPanel/AddRecyclePoint.tsx
+++ src/pages/AdminPanel/AddRecyclePoint.tsx
@@ -1,71 +1,72 @@
-import { Button, Center, FormControl, Input } from '@chakra-ui/react';
-import React, { useEffect, useState } from 'react';
-import Map from '../../components/organisms/Map/Map';
-import { useForm } from 'react-hook-form';
-import { api } from '../../shared/api/api';
-import { RecyclePoint } from '../../shared/types';
+import { Button, Center, FormControl, HStack, Input, Select } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import Map from "../../components/organisms/Map/Map";
+import { useForm } from "react-hook-form";
+import { api } from "../../shared/api/api";
+import { RecyclePoint } from "../../shared/types";
+import { useMutation } from "react-query";
 
 type AddRecyclePointProps = {
     currentPoint?: RecyclePoint;
 };
 
-export const AddRecyclePoint: React.FC<AddRecyclePointProps> = ({
-    currentPoint,
-}) => {
+export const AddRecyclePoint: React.FC<AddRecyclePointProps> = ({ currentPoint }) => {
+    const mutation = useMutation(api.admin.newRecyclePoint, {
+        onSuccess: () => alert("Geri Donusum Noktasi Basariyla Eklendi"),
+    });
+
     const [currentLoc, setCurrentLoc] = useState<RecyclePoint>({
-        recyclePointDetail: 'Geri donusum noktasi',
+        recyclePointDetail: "Geri donusum noktasi",
         lat: 39.904239006864785,
         lng: 32.87195490942385,
+        recyclyPointPlaceType: "Plastik",
     });
 
     useEffect(() => {
         if (currentPoint) {
             setCurrentLoc(currentPoint);
-            console.log(currentPoint);
-            console.log(currentLoc);
         }
     }, [currentPoint]);
     const { handleSubmit, register, formState } = useForm();
 
-    function onSubmit(values: {
-        recyclePointDetail: string;
-        lat: number;
-        lng: number;
-    }) {
-        //TODO react-query
+    function onSubmit(values: any) {
+        console.log(values);
         values.lat = currentLoc.lat;
         values.lng = currentLoc.lng;
-        api.admin.newRecyclePoint(values);
+        mutation.mutate(values);
     }
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
             <FormControl>
-                <Map
-                    setCurrentLoc={setCurrentLoc}
-                    addPoint={true}
-                    currentLoc={currentLoc}
-                />
+                <Map setCurrentLoc={setCurrentLoc} addPoint={true} currentLoc={currentLoc} />
             </FormControl>
-            <FormControl>
-                <Input
-                    m={2}
-                    name='recyclePointDetail'
-                    placeholder='Detay Giriniz'
-                    focusBorderColor='lime'
-                    borderColor='green.700'
-                    isRequired={true}
-                    ref={register({ required: true })}
-                />
-            </FormControl>
+            <HStack margin="4" spacing="8">
+                <FormControl>
+                    <Input
+                        m={2}
+                        name="recyclePointDetail"
+                        placeholder="Detay Giriniz"
+                        focusBorderColor="lime"
+                        borderColor="green.700"
+                        isRequired={true}
+                        ref={register({ required: true })}
+                    />
+                </FormControl>
+                <FormControl>
+                    <Select variant="filled" name="recyclePointPlaceType" ref={register({ required: true })}>
+                        <option value="Plastik">Plastik</option>
+                        <option value="Elektronik">Elektronik</option>
+                        <option value="Kağıt">Kağıt</option>
+                        <option value="Pil">Pil</option>
+                        <option value="Cam">Cam</option>
+                        <option value="Yağ">Yağ</option>
+                    </Select>
+                </FormControl>
+            </HStack>
             <Center>
-                <Button
-                    colorScheme='teal'
-                    m={2}
-                    isLoading={formState.isSubmitting}
-                    type='submit'
-                >
-                    Uygula
+                <Button colorScheme="teal" m={2} isLoading={formState.isSubmitting} type="submit">
+                    Ekle
                 </Button>
             </Center>
         </form>
