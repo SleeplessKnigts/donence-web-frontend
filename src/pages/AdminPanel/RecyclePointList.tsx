3b6eba6062d63613938f1f3da43638d79aaf241a
Arif <marfsahin@gmail.com>
2021-04-06 21:39:02 +0300

    refactor: recycle point list
    -add useQuery
    -remove modal on updating point
    -add tab for display points on map

--- src/pages/AdminPanel/RecyclePointList.tsx
+++ src/pages/AdminPanel/RecyclePointList.tsx
@@ -19,6 +19,12 @@ import {
     ModalFooter,
     useDisclosure,
     Container,
+    Spinner,
+    Tabs,
+    TabList,
+    Tab,
+    TabPanels,
+    TabPanel,
 } from '@chakra-ui/react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { api } from '../../shared/api/api';
@@ -28,35 +34,32 @@ import { faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import AddRecyclePoint from './AddRecyclePoint';
 import { GoogleMap } from '@react-google-maps/api';
 import { MapPoints } from '../../components/molecules/MapPoints';
+import { useQuery } from 'react-query';
 
+import { useHistory } from 'react-router';
 export const RecyclePointList = () => {
-    //TODO useQuery
-    // const {data, error} = useQuery("points", api.admin.getRecyclePoints);
-    // if(error) return <span>Error when loading data</span>
-    // if(!data) return <span>Loading...</span>
+    let history = useHistory();
+    const { data: recyclePointList, isFetched } = useQuery(
+        'getRecPointList',
+        api.admin.getRecyclePoints
+    );
     const [points, setPoints] = useState<RecyclePoint[]>([]);
     const [clickedPoint, setClickedPoint] = useState<RecyclePoint>();
     const [showUpdatePopup, setShowUpdatePopup] = useState(false);
     // useDisclosure is a custom hook used to help handle common open, close, or toggle scenarios.
     const { isOpen, onOpen, onClose } = useDisclosure();
 
-    useEffect(() => {
-        api.admin
-            .getRecyclePoints()
-            .then((response) => {
-                setPoints(response);
-            })
-            .catch((e) => console.error(e));
-    }, []);
-
     const handleUpdateClick = (point: RecyclePoint) => {
-        setClickedPoint(point);
-        setShowUpdatePopup(true);
-        console.log(point);
-        onOpen();
+        history.push({
+            pathname: '/admin/geri-donusum-noktasi-ekle',
+            state: { currentPoint: point },
+        });
     };
-    return (
-        <Container centerContent maxW='container.lg'>
+
+    let component = <Spinner size='lg' />;
+    let map = null;
+    if (isFetched) {
+        component = (
             <Grid m={4}>
                 <Table
                     variant='simple'
@@ -69,14 +72,18 @@ export const RecyclePointList = () => {
                     <Thead>
                         <Tr>
                             <Th>Geri Dönüşüm Noktasi isimleri</Th>
+                            <Th>Materyal</Th>
                             <Th isNumeric>Latitude</Th>
                             <Th isNumeric>Longitude</Th>
+                            <Th>Güncelle</Th>
+                            <Th>Sil</Th>
                         </Tr>
                     </Thead>
                     <Tbody>
-                        {points.map((point) => (
+                        {recyclePointList?.map((point) => (
                             <Tr>
                                 <Td>{point.recyclePointDetail}</Td>
+                                <Td>{point.recyclyPointPlaceType}</Td>
                                 <Td isNumeric>{point.lat}</Td>
                                 <Td isNumeric>{point.lng}</Td>
                                 <Td>
@@ -106,39 +113,31 @@ export const RecyclePointList = () => {
                     <Tfoot>
                         <Tr>
                             <Th>Geri Dönüşüm Noktasi İsimleri</Th>
+                            <Th>Materyal</Th>
                             <Th isNumeric>Latitude</Th>
                             <Th isNumeric>Longitude</Th>
+                            <Th>Güncelle</Th>
+                            <Th>Sil</Th>
                         </Tr>
                     </Tfoot>
                 </Table>
-                {showUpdatePopup && (
-                    <Modal isOpen={isOpen} onClose={onClose} size='full'>
-                        <ModalOverlay />
-                        <ModalContent>
-                            <ModalHeader>
-                                Geri Dönüşüm Noktasi Güncelleme
-                            </ModalHeader>
-                            <ModalCloseButton />
-                            <ModalBody>
-                                <AddRecyclePoint currentPoint={clickedPoint} />
-                            </ModalBody>
-
-                            <ModalFooter>
-                                <Button
-                                    colorScheme='blue'
-                                    mr={3}
-                                    onClick={onClose}
-                                >
-                                    Close
-                                </Button>
-                                <Button variant='ghost'>
-                                    Secondary Action
-                                </Button>
-                            </ModalFooter>
-                        </ModalContent>
-                    </Modal>
-                )}
             </Grid>
+        );
+        map = <MapPoints recyclePoints={recyclePointList} />;
+    }
+    return (
+        <Container maxW='container.lg'>
+            <Tabs m={2} variant='soft-rounded' colorScheme='green'>
+                <TabList>
+                    <Tab>Geri Dönüşüm Noktaları</Tab>
+                    <Tab>Haritada Görüntüle</Tab>
+                </TabList>
+
+                <TabPanels>
+                    <TabPanel>{component}</TabPanel>
+                    <TabPanel>{map}</TabPanel>
+                </TabPanels>
+            </Tabs>
         </Container>
     );
 };
